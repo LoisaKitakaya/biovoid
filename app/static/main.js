b33cbca33d5fde90593da437300543a2d5ffc25c
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-12 22:18:10 +0300

    admin panel styling

--- app/static/main.js
+++ app/static/main.js
@@ -3,3 +3,10 @@ const clearNotification = () => {
 
   notificationBox.remove();
 };
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
