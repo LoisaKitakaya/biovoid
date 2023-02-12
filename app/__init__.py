f5c8e380c376ed104d6381b41c868b7312e8256a
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-12 13:55:39 +0300

    created custom command to create superuser account

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import click
 from flask import Flask
 
 from config import Config
@@ -16,5 +17,13 @@ def create_app(config_class=Config):
     from app.core import bp as main_blueprint
     
     app.register_blueprint(main_blueprint)
+
+    @app.cli.command("createsuperuser")
+    def create_superuser():
+
+        name = click.prompt("Enter superuser name")
+        email = click.prompt("Enter superuser email")
+        password = click.prompt("Enter superuser password", hide_input=True)
+        password_2 = click.prompt("Confirm superuser password", hide_input=True)
 
     return app
