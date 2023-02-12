6842c238567802df9aa650f89a88599ea2a6e805
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-12 13:59:30 +0300

    added http methods to app core routes

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,17 +1,17 @@
 from flask import render_template
 from app.core import bp
 
-@bp.route('/')
+@bp.route('/', methods=['GET'])
 def home():
 
     return render_template('home.html')
 
-@bp.route('/admin/')
+@bp.route('/admin/', methods=['GET', 'POST'])
 def admin():
 
     return render_template('admin/admin.html')
 
-@bp.route('/sign_in/')
+@bp.route('/sign_in/', methods=['GET', 'POST'])
 def sign_in():
 
     return render_template('admin/sign_in.html')
