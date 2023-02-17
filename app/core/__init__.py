b4527b362b8b95346367fc5d3815bf5a5ea47f39
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-17 18:39:22 +0300

    removed flask login settings

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,10 +1,5 @@
 from flask import Blueprint
-from app.extensions import login_manager
 
 bp = Blueprint('core', __name__)
 
 from app.core import routes
-
-login_manager.login_view = 'core.sign_in'
-login_manager.login_message_category = "info"
-login_manager.login_message = "Please login to access the admin panel."
