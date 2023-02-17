91de179e7c087294e0eb6112bdf3769769efeeaf
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-17 18:39:41 +0300

    added flask login settings

--- app/extensions.py
+++ app/extensions.py
@@ -5,3 +5,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+
+login_manager.login_view = 'core.sign_in'
+login_manager.login_message_category = "info"
+login_manager.login_message = "Please login to access this page."
