917d505d5d0d54aaac578a5f3d76124ce912f287
Freedom Loisa <kitakayaloisa@gmail.com>
2023-02-23 10:18:12 +0300

    routes to implement AI art generation

--- app/site/routes.py
+++ app/site/routes.py
@@ -8,19 +8,43 @@ def home():
 
     return render_template('site/home.html', this_user=current_user)
 
+response = {}
+
 @bp.route('/app/generate_art/', methods=['GET', 'POST'])
 def generate_art():
 
+    if request.method == 'POST':
+
+        style = request.form.get('style')
+        text_prompt = request.form.get('text_prompt')
+        number = request.form.get('number')
+        size = request.form.get('size')
+
+        prompt = f"style: {style}. Description: {text_prompt}"
+
+        image_object = AIArtGenerator(
+            prompt=prompt,
+            n=int(number),
+            size=size
+        )
+
+        response.update({"data": image_object.generate_art()})
+        
+        print(response['data'])
+
+        return redirect(url_for('site.generate_art'))
+
     return render_template(
         'site/app/generate_art.html',
-        this_user=current_user
+        this_user=current_user,
+        generated_response=response
     )
 
-@bp.route('/app/photo_to_image/', methods=['GET', 'POST'])
-def photo_to_image():
+@bp.route('/app/photo_to_art/', methods=['GET', 'POST'])
+def photo_to_art():
 
     return render_template(
-        'site/app/photo_to_image.html',
+        'site/app/photo_to_art.html',
         this_user=current_user
     )
 
@@ -29,5 +53,13 @@ def edit_image():
 
     return render_template(
         'site/app/edit_image.html',
+        this_user=current_user
+    )
+
+@bp.route('/app/image_variation/', methods=['GET', 'POST'])
+def image_variation():
+
+    return render_template(
+        'site/app/image_variation.html',
         this_user=current_user
     )
